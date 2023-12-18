ca116993c2d720239e7f481f1d896988625de31f
Akhya Chandra <77987097+AkhyaChandra@users.noreply.github.com>
2023-12-18 20:43:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text", {
+  strings:["Campare Flights & Hotels", "Check Reviews about Various Locations", "Visit locations at the best time of the Year"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
